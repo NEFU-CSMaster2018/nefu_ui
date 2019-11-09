96d9fa45bf61180e3ffedb587ca430397aea71e4
AdamLambet <nefu_yanglin@163.com>
2019-11-09 23:28:42 +0800

    homepage init
    homepage init

---
+++ src/components/HomePage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home-container">
+    <div class="picture-wall"></div>
+    <div class="introduction"></div>
+    <div class="contanct-info"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.home-container {
+  height: 100%;
+  width: 100%;
+  background-image: url(../../assets/images/homepage/pagebg1.jpg);
+  background-size: cover;
+}
+</style>
